908071a55df532c0b2e934495231a6a2e8ca9140
Tanishk Tyagi <tanishkt17@gmail.com>
2026-04-19 01:03:00 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -7,23 +7,24 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
-
 import Weather from "./pages/Weather";
 import Contact from "./pages/Contact";
+import Farmers from "./pages/Farmers"; // <-- Added Farmers import
+
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/login" element={<Login/>}/>
-  <Route path="/signup" element={<Signup/>}/>
-  <Route path="/dashboard" element={<Dashboard/>}/>
-  <Route path="/products" element={<Products/>}/>
-  
-  <Route path="/weather" element={<Weather />} />
-  <Route path="/contact" element={<Contact />} />
-</Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/farmers" element={<Farmers />} /> {/* <-- Added Farmers route */}
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
